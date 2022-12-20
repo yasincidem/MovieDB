69fb2481443be6b7441f7dd4def407194087141d
Yasin Çidem <yasincidem@gmail.com>
2022-12-20 21:15:07 +0300

    Disable darkTheme

--- app/src/main/java/com/yasincidem/moviedb/ui/theme/Theme.kt
+++ app/src/main/java/com/yasincidem/moviedb/ui/theme/Theme.kt
@@ -2,7 +2,6 @@ package com.yasincidem.moviedb.ui.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -10,7 +9,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -39,9 +37,9 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun MovieDBTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
