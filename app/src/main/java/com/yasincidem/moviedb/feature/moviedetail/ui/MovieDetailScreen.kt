70cbc683277a7d4e98605dfa21d17cc16b0b9b75
Yasin Çidem <yasincidem@gmail.com>
2022-12-21 12:37:51 +0300

    Add shareIntent and browserIntent

--- app/src/main/java/com/yasincidem/moviedb/feature/moviedetail/ui/MovieDetailScreen.kt
+++ app/src/main/java/com/yasincidem/moviedb/feature/moviedetail/ui/MovieDetailScreen.kt
@@ -1,29 +1,31 @@
 package com.yasincidem.moviedb.feature.moviedetail.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material.icons.rounded.Share
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -62,6 +64,25 @@ fun MovieDetailScreen(
     var nonSafePalette by remember { mutableStateOf<Palette?>(null) }
     val animatedColor = remember { Animatable(Color.White) }
     val movieDetail by viewModel.movieDetail.collectAsStateWithLifecycle()
+
+    val browserIntent = remember {
+        derivedStateOf {
+            Intent(Intent.ACTION_VIEW, Uri.parse(movieDetail.homepage))
+        }
+    }
+
+    val shareIntent = remember {
+        derivedStateOf {
+            Intent.createChooser(Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    "Here is a movie recommendation: ${movieDetail.title} \n\n ${movieDetail.homepage}"
+                )
+                type = "text/plain"
+            }, null)
+        }
+    }
 
     LaunchedEffect(Unit) {
         viewModel.fetchMovieDetail(id)
@@ -113,16 +134,36 @@ fun MovieDetailScreen(
                         )
                     }
 
-                    FilledIconButton(
-                        modifier = Modifier
-                            .align(Alignment.BottomEnd)
-                            .padding(8.dp),
-                        onClick = { }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Rounded.Share,
-                            contentDescription = "Share ${movieDetail.title}"
-                        )
+                    if (movieDetail.homepage.isNotEmpty()) {
+                        Row(
+                            modifier = Modifier.align(Alignment.BottomEnd)
+                        ) {
+                            FilledIconButton(
+                                modifier = Modifier
+                                    .padding(8.dp),
+                                onClick = {
+                                    context.startActivity(browserIntent.value)
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Rounded.Search,
+                                    contentDescription = "Open ${movieDetail.title} in browser"
+                                )
+                            }
+
+                            FilledIconButton(
+                                modifier = Modifier
+                                    .padding(8.dp),
+                                onClick = {
+                                    context.startActivity(shareIntent.value)
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Rounded.Share,
+                                    contentDescription = "Share ${movieDetail.title}"
+                                )
+                            }
+                        }
                     }
                 }
             }
